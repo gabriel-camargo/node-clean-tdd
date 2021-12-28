4f99da40b7682e792f3714f9b31f408613f2ccc8
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2021-12-28 15:16:04 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presensation/controllers/signup.spec.ts
+++ src/presensation/controllers/signup.spec.ts
@@ -5,7 +5,6 @@ describe('SignUp COntroller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@email.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
@@ -13,5 +12,6 @@ describe('SignUp COntroller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })

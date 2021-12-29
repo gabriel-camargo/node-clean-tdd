45d81a5a3b1b4c4bf278bc146ce3a3a3d9d10907
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2021-12-29 14:57:37 -0300

    refactor: move all sign-up protocols to a single file

--- src/presensation/controllers/signup/signup.ts
+++ src/presensation/controllers/signup/signup.ts
@@ -1,7 +1,6 @@
-import { AddAccount } from '../../domain/usecases/add-acount'
-import { InvalidParamError, MissingParamError } from '../errors'
-import { badRequest, serverError } from '../helpers/http-helper'
-import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse, AddAccount } from './signup-protocols'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
   constructor (

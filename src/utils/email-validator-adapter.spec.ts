55bccd8467101a9708fabd2cd6bc91db3c6499d7
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2021-12-30 16:30:07 -0300

    style: remove blank lines

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -15,16 +15,14 @@ describe('EmailValidator Adapter', () => {
   test('Should return true if validator returns true', () => {
     const sut = makeSut()
     const isValid = sut.isValid('valid_email@email.com')
-
     expect(isValid).toBe(true)
   })
 
   test('Should return false if validator returns false', () => {
     const sut = makeSut()
-
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const isValid = sut.isValid('invalid_email@email.com')
 
+    const isValid = sut.isValid('invalid_email@email.com')
     expect(isValid).toBe(false)
   })
 

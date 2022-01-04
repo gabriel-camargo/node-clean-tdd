8605a352f1de5a6259bca8a79039b8a3e9b25c0b
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2022-01-03 21:00:55 -0300

    feat: ensure bcrypt adapter return a hash on success

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -4,7 +4,7 @@ import { Encrypter } from '../../data/protocols/encrypter'
 export class BcryptAdapter implements Encrypter {
   constructor (private readonly salt: number) {}
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
-    return await new Promise(resolve => resolve(value))
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
   }
 }

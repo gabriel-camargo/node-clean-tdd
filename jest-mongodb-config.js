1fc3f332a13ce53a0a1181d8ac269883c6682eb2
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2022-01-05 20:51:16 -0300

    chore: change jest mongodb config

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -4,7 +4,9 @@ module.exports = {
       version: '4.0.3',
       skipMD5: true
     },
-    autoStart: false,
-    instance: {}
+    instance: {
+      dbName: 'jest'
+    },
+    autoStart: false
   }
 }

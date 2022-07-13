96a55d1712f7a105aa6173e1d55fc78cebffac4e
gabriel-camargo <gabriel.rodrigocam@gmail.com>
2022-07-13 09:25:31 -0300

    refactor(composite-validation): move sut creation to a factory method

--- src/presensation/helpers/validators/validation-composite.spec.ts
+++ src/presensation/helpers/validators/validation-composite.spec.ts
@@ -2,19 +2,35 @@ import { MissingParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
-const makeSut = (): ValidationComposite => (
-  new ValidationComposite([new ValidationStub()])
-)
-
-class ValidationStub implements Validation {
-  validate (input: any): Error {
-    return new MissingParamError('field')
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null
+    }
   }
+
+  return new ValidationStub()
+}
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
 }
 
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+
+  return {
+    sut,
+    validationStub
+  }
+}
 describe('Validation Composite', () => {
   test('Should return an error if any fails', () => {
-    const sut = makeSut()
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      new MissingParamError('field')
+    )
     const error = sut.validate({
       field: 'any_value'
     })

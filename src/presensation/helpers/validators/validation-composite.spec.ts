5a7652bef8138c8ea84cac1fd6bac34152b4a185
gabriel camargo <gabriel.camargo@aiqfome.com>
2022-07-13 09:55:40 -0300

    test(composite-validation): ensure composite not return if validation succeeds

--- src/presensation/helpers/validators/validation-composite.spec.ts
+++ src/presensation/helpers/validators/validation-composite.spec.ts
@@ -50,4 +50,12 @@ describe('Validation Composite', () => {
     })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return an error if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({
+      field: 'any_value'
+    })
+    expect(error).toBeFalsy()
+  })
 })

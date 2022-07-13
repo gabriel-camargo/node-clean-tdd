9e71cbe2faea68f115765d299e9189ffcb64df34
gabriel camargo <gabriel.camargo@aiqfome.com>
2022-07-13 09:02:44 -0300

    test(required-field-validation): ensure that returns null if validation succeeds

--- src/presensation/helpers/validators/required-field-validations.spec.ts
+++ src/presensation/helpers/validators/required-field-validations.spec.ts
@@ -2,9 +2,15 @@ import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validaton'
 
 describe('RequiredField Validations', () => {
-  test('Should returna a MissingParamError if validation fails', () => {
+  test('Should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('any_field')
     const error = sut.validate({ other_field: 'any_other_field' })
     expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  test('Should not return if validations succeds', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({ any_field: 'any_field' })
+    expect(error).toBeFalsy()
   })
 })

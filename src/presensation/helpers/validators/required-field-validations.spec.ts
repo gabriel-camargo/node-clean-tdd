da16ce3ce644330293f189491f08906787c3858d
gabriel-camargo <gabriel.rodrigocam@gmail.com>
2022-07-13 09:04:16 -0300

    refactor(required-filed-validation): move sut creation to a factory method

--- src/presensation/helpers/validators/required-field-validations.spec.ts
+++ src/presensation/helpers/validators/required-field-validations.spec.ts
@@ -1,15 +1,19 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validaton'
 
+const makeSut = (): RequiredFieldValidation => (
+  new RequiredFieldValidation('any_field')
+)
+
 describe('RequiredField Validations', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = makeSut()
     const error = sut.validate({ other_field: 'any_other_field' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
 
   test('Should not return if validations succeds', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = makeSut()
     const error = sut.validate({ any_field: 'any_field' })
     expect(error).toBeFalsy()
   })

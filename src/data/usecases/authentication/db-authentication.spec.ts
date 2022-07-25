fbdbf76813ec4ba7ac1b208e4a2da9514d12ed3d
gabriel-camargo <gabriel.rodrigocam@gmail.com>
2022-07-25 17:15:08 -0300

    test(auth): ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -56,4 +56,13 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(
+      new Promise(resolve => resolve(null))
+    )
+    const token = await sut.auth(makeFakeAuthentication())
+    expect(token).toBeNull()
+  })
 })

2771b436e39f48b401cf2696a86592a3f1cf3856
gabriel camargo <gabriel.rodrigocam@gmail.com>
2022-08-23 19:44:29 -0300

    test: ensure AccountMongoRepository updates accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -59,4 +59,18 @@ describe('Account Mongo Repository', () => {
 
     expect(account).toBeNull()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const result = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+
+    await sut.updateAccessToken(result.insertedId.toString(), 'any_token')
+    const account = await accountCollection.findOne({ _id: result.insertedId })
+    expect(account).toBeTruthy()
+    expect(account?.accessToken).toBe('any_token')
+  })
 })

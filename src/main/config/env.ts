6de843c68db26136567a99f505d8120c53eacc94
gabriel camargo <gabriel.rodrigocam@gmail.com>
2022-09-27 20:50:16 -0300

    chore: config docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://gabes:123@0.0.0.0:27017/',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://db:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'hAi*jj2moA092kA'
 }
